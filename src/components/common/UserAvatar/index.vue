c799b2857c5dfc746d8d5be94dcfac7e4589d4ba
Pom <1078889045@qq.com>
2023-02-14 14:11:56 +0800

    fix: update

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -9,7 +9,7 @@ import { GithubSite } from '@/components/custom'
     </div>
     <div class="ml-2">
       <h2 class="font-bold text-md">
-        ChenZhaoYu
+        POM ZHU
       </h2>
       <p class="text-xs text-gray-500">
         <GithubSite />
