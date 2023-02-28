2c0dfc2fb576a97547ff5bfae72aab4ca46677db
Pom <1078889045@qq.com>
2023-02-28 14:26:58 +0800

    fix: update

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-center text-neutral-500">
-    <span>❤️ Star on</span>
+  <div class="text-neutral-400">
+    <span>Star on</span>
     <a href="https://github.com/1078889045/chatgpt-web" target="_blank" class="text-blue-500">
       GitHub
     </a>
